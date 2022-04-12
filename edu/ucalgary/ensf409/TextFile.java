f3c39c799fbbafc0aa0963c7c0d229b305a20fd1
Nour Amer <84950398+NoureldinAmer@users.noreply.github.com>
2022-04-12 06:52:21 -0600

    fixed TextFile.java and TextOutput.java

--- edu/ucalgary/ensf409/TextFile.java
+++ edu/ucalgary/ensf409/TextFile.java
@@ -1,20 +1,5 @@
 package edu.ucalgary.ensf409;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-
 public interface TextFile {
-    default public void generateFile(String Order, String output)
-    {
-        try {
-            BufferedWriter wr = new BufferedWriter(new FileWriter(output));
-            wr.write(Order);
-            wr.close();
-
-        } catch (IOException e) {
-            System.out.println("Cannot create output file");
-        }
-        
-    }
+    public void generateFile(String output);
 }
--- edu/ucalgary/ensf409/TextOutput.java
+++ edu/ucalgary/ensf409/TextOutput.java
@@ -1,5 +1,8 @@
 package edu.ucalgary.ensf409;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class TextOutput implements TextFile {
@@ -26,5 +29,74 @@ public class TextOutput implements TextFile {
     public Order getOrder()
     {
         return this.ORDER;
+    }
+
+    @Override
+    default public void generateFile(String outputFileName) //ie including .txt
+    {
+        try {
+            BufferedWriter wr = new BufferedWriter(new FileWriter(output));
+            wr.write("Food BanK \n" + "Hamoer Order Form \n \n" + "Name:\nDate:\n"
+            + "\n Original Request \n" );
+            
+            ArrayList <Family> families = ORDER.getFamilies();
+            int i;
+
+            for(i = 0; i < families.size(); i++)
+            {
+                Family family = families[i];
+                String adultMale;
+                String adultFemale;
+                String over8;
+                String under8;
+
+                if(family.getAdultMale() == 0)
+                {
+                    adultMale = "";
+                } else {
+                    adultMale = family.getAdultMale() + " Adult Male, ";
+                }
+
+                if (family.getAdultFemale() == 0) {
+                    adultFemale = "";
+                } else {
+                    adultFemale = family.getAdultMale() + " Adult female, ";
+                }
+
+                if (family.getChildOver8() == 0) {
+                    over8 = "";
+                } else {
+                    over8 = family.getAdultMale() + " Child Over 8, ";
+                }
+
+                if (family.getChildUnder8() == 0) {
+                    under8 = "";
+                } else {
+                    under8 = family.getChildUnder8() + " Child Under 8";
+                }
+                
+                wr.write("Hamper " + i+1 + ": " +  adultMale + adultFemale +
+                over8 + under8 + '\n');
+            }
+            
+            for(int j = 0; j < families.size(); j++)
+            {
+                Family family = families[j];
+                Hamper hamper = family.getHamper();
+                ArrayList<Food> foodItems= hamper.getFoodItems();
+                wr.write("Hamper " + j+1 + "items:\n");
+                for(int k = 0; k < foodItems.size(); k ++)
+                {
+                    Food foodItem = foodItems[k];
+                    wr.write(foodItem.getItemId() + "\t" + foodItem.getName() + "\n");
+                }
+                wr.write("\n\n"); //skip 2 lines
+            }
+            wr.close();
+
+        } catch (IOException e) {
+            System.out.println("Cannot create output file");
+        }
+        
     }
 }
