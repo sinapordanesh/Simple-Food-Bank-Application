08eece2c9268b49bee0695ad3650d8061762f456
Nour Amer <84950398+NoureldinAmer@users.noreply.github.com>
2022-04-13 16:19:34 -0600

    implemented NutritionalItems.java

--- edu/ucalgary/ensf409/ClientType.java
+++ edu/ucalgary/ensf409/ClientType.java
@@ -1,28 +1,22 @@
 package edu.ucalgary.ensf409;
 
 public enum ClientType {
-    ADULTMALE {
-        @Override
-        public String toString() {
-            return "ADULTMALE";
+    ADULTMALE, ADULTFEMALE, CHILDOVER8, CHILDUNDER8;
+    public NutritionalItems getNutritionalItems(int wG, int fV, int p, int o, int c) {
+        switch(this) {
+            case ADULTMALE:
+                NutritionalItems adultMale = new NutritionalItems(wG, fV, p, o, c);
+                return adultMale;
+            case ADULTFEMALE:
+                NutritionalItems adultFemale = new NutritionalItems(wG, fV, p, o, c);
+                return adultFemale;
+            case CHILDOVER8:
+                NutritionalItems childUnder8 = new NutritionalItems(wG, fV, p, o, c);
+                return childUnder8;
+            case CHILDUNDER8:
+                NutritionalItems childOver8 = new NutritionalItems(wG, fV, p, o, c);
+                return childOver8;
         }
-    },
-    ADULTFEMALE {
-        @Override
-        public String toString() {
-            return "ADULTFEMALE";
-        }
-    },
-    CHILDOVER8 {
-        @Override
-        public String toString() {
-            return "CHILDOVER8";
-        }
-    },
-    CHILDUNDER8 {
-        @Override
-        public String toString() {
-            return "CHILDUNDER8";
-        }
+        return null;
     }
 }
--- edu/ucalgary/ensf409/NutritionalNeedsMap.java
+++ edu/ucalgary/ensf409/NutritionalNeedsMap.java
@@ -1,7 +1,42 @@
 package edu.ucalgary.ensf409;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.HashMap;
 
 public class NutritionalNeedsMap {
-    HashMap <String, String> NutritionalNeeds = new HashMap<>();
+    HashMap <String, NutritionalItems> TABLE; 
+    public NutritionalNeedsMap(Connection dbConnect)
+    {
+        Statement myStmt;
+        ResultSet results;
+
+        try {
+            myStmt = dbConnect.createStatement();
+
+            results = myStmt.executeQuery("SELECT * FROM DAILY_CLIENT_NEEDS WHERE Client = 'Adult Male");
+            TABLE.put("ADULTMALE",ClientType.ADULTMALE.getNutritionalItems(results.getInt("WholeGrains"), 
+            results.getInt("FruitVeggies"), results.getInt("Protein"), 
+            results.getInt("Other"), results.getInt("Calories")));
+
+            results = myStmt.executeQuery("SELECT * FROM DAILY_CLIENT_NEEDS WHERE Client = 'Adult female");
+            TABLE.put("ADULTFEMALE",ClientType.ADULTFEMALE.getNutritionalItems(results.getInt("WholeGrains"), 
+            results.getInt("FruitVeggies"), results.getInt("Protein"), 
+            results.getInt("Other"), results.getInt("Calories")));
+
+            TABLE.put("CHILDOVER8",ClientType.CHILDOVER8.getNutritionalItems(results.getInt("WholeGrains"), 
+            results.getInt("FruitVeggies"), results.getInt("Protein"), 
+            results.getInt("Other"), results.getInt("Calories")));
+
+            TABLE.put("CHILUNDER8",ClientType.CHILDUNDER8.getNutritionalItems(results.getInt("WholeGrains"), 
+            results.getInt("FruitVeggies"), results.getInt("Protein"), 
+            results.getInt("Other"), results.getInt("Calories")));
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+    }
 }
