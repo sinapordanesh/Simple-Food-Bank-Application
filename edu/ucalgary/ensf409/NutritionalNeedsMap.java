fd53cd0d8413458d49c770289b0127f83e0ecc66
Nour Amer <84950398+NoureldinAmer@users.noreply.github.com>
2022-04-13 16:24:32 -0600

    fixed NutritionalItemsMap

--- edu/ucalgary/ensf409/NutritionalNeedsMap.java
+++ edu/ucalgary/ensf409/NutritionalNeedsMap.java
@@ -16,22 +16,22 @@ public class NutritionalNeedsMap {
         try {
             myStmt = dbConnect.createStatement();
 
-            results = myStmt.executeQuery("SELECT * FROM DAILY_CLIENT_NEEDS WHERE Client = 'Adult Male' ");
+            results = myStmt.executeQuery("SELECT * FROM DAILY_CLIENT_NEEDS WHERE Client = 'Adult Male'");
             TABLE.put("ADULTMALE",ClientType.ADULTMALE.getNutritionalItems(results.getInt("WholeGrains"), 
             results.getInt("FruitVeggies"), results.getInt("Protein"), 
             results.getInt("Other"), results.getInt("Calories")));
 
-            results = myStmt.executeQuery("SELECT * FROM DAILY_CLIENT_NEEDS WHERE Client = 'Adult Adult Female' ");
+            results = myStmt.executeQuery("SELECT * FROM DAILY_CLIENT_NEEDS WHERE Client = 'Adult Adult Female'");
             TABLE.put("ADULTFEMALE",ClientType.ADULTFEMALE.getNutritionalItems(results.getInt("WholeGrains"), 
             results.getInt("FruitVeggies"), results.getInt("Protein"), 
             results.getInt("Other"), results.getInt("Calories")));
 
-            results = myStmt.executeQuery("SELECT * FROM DAILY_CLIENT_NEEDS WHERE Client = 'Child over 8' ");
+            results = myStmt.executeQuery("SELECT * FROM DAILY_CLIENT_NEEDS WHERE Client = 'Child over 8'");
             TABLE.put("CHILDOVER8",ClientType.CHILDOVER8.getNutritionalItems(results.getInt("WholeGrains"), 
             results.getInt("FruitVeggies"), results.getInt("Protein"), 
             results.getInt("Other"), results.getInt("Calories")));
 
-            results = myStmt.executeQuery("SELECT * FROM DAILY_CLIENT_NEEDS WHERE Client = 'Child under 8' ");
+            results = myStmt.executeQuery("SELECT * FROM DAILY_CLIENT_NEEDS WHERE Client = 'Child under 8'");
             TABLE.put("CHILUNDER8",ClientType.CHILDUNDER8.getNutritionalItems(results.getInt("WholeGrains"), 
             results.getInt("FruitVeggies"), results.getInt("Protein"), 
             results.getInt("Other"), results.getInt("Calories")));
